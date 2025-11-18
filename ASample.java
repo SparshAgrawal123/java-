a04890fd3436e732a9883f39e11e51083b9511ce
SparshAgrawal123 <sparshagrawal.cs24@bmsce.ac.in>
2025-11-18 10:44:01 +0530

    Add files via upload

---
+++ ASample.java
@@ -0,0 +1,8 @@
+package SAMPLE;
+public class ASample{
+
+    public void show(){
+
+        System.out.println("hello");
+    }
+}
---
+++ External.java
@@ -0,0 +1,12 @@
+package SEE;
+
+import CIE.Personal;
+
+public class External extends Personal {
+    public int[] seeMarks = new int[5];
+
+    public External(String usn, String name, int sem) {
+        super(usn, name, sem);
+    }
+}
+
